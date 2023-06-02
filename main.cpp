36b33ef3764d1306f67e18adc5ede5a9f71f2ae3
Alex Bogushenko <bogushenkoas@ukr.net>
2023-06-02 15:12:05 +0300

    2 commit

--- main.cpp
+++ main.cpp
@@ -5,29 +5,69 @@
 
 using namespace std;
 
+double calcTime(time_t start, time_t end)
+{
+  return difftime(end, start) / 3600;
+}
+
 main()
  {
  string command = "";
- string task = "";
+ string current_task = "";
  time_t start = 0;
  time_t end = 0;
  time_t total = 0;
 
  while(command != "exit")
  {
-   cout << "enter command:";
+   cout << "введите команду:";
    cin >> command;
 
    if(command == "begin")
    {
-     cout << "enter name task:";
-     cin >> task;
+
+    if(current_task != "")
+     {
+      end = time(NULL);
+      double time_spent = calcTime(start,end);
+      total += (time_t)time_spent;
+      cout << "задача " << current_task << " завершена за " << time_spent << " часов" << endl;
+     }
+
+     cout << "введите название задачи:";
+     cin >> current_task;
      start = time(NULL);
-     cout << "task tracking: " << task << endl;
+     cout << "отслеживание задачи " << current_task << " начато" << endl;
    }
+
+     else if(command == "end")
+      {
+        if(current_task != "")
+        {
+          end = time(NULL);
+          double time_spent = calcTime(start, end);
+          total += (time_t)time_spent;
+          cout << "задача " << current_task << " завершена за " << time_spent << " часов" << endl;
+          current_task = "";
+        }
+
+      }
+
+        else if (command == "status")
+        {
+          if(current_task != "")
+           {
+             cout << "currently tracking task: " << current_task << endl;
+           }
+
+           cout << "total spent on all tasks: " << total << " second" << endl;
+          
+        }
    
  }
  
-cout << "program completed";
+cout << "программа завершена.";
+
+return 0;
   
  }
